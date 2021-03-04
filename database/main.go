30c4cfd36cbbb2e1da7cce352ebdb23e0ce96f61
kumin <minhhoang.it.hut@gmail.com>
2021-03-04 18:58:45 +0700

    init field values

--- database/main.go
+++ database/main.go
@@ -8,16 +8,22 @@ import (
 	"github.com/my-packages/database/mysql"
 )
 
+func InitSectionFields(ctx context.Context) {
+}
+
+func InitFieldValues(ctx context.Context) {
+}
+
 func main() {
 	initor := mysql.NewInitDatabase()
 	ctx := context.Background()
-	data, err := initor.ReadJsondataFromFile("./segment_fields.json")
+	data, err := initor.ReadJsondataFromFile("./mysql/targeting_fields.json")
 	if err != nil {
 		log.Fatalf("Read data from file error: %v", err)
 		os.Exit(1)
 	}
 	dao := &mysql.SectionFieldsDAO{
-		Section: "segment",
+		Section: "targeting",
 		Fields:  data,
 	}
 	if err = initor.InsertBatches(ctx, dao); err != nil {
